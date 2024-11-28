f683955ac86b951d1862b608d7913c70027852b5
amelhasic23 <117470943+amelhasic23@users.noreply.github.com>
2024-11-28 08:50:37 +0100

    Commit

---
+++ Program koji ispisuje sve parne brojeve između dva uneta broja/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Unesite prvi broj: ");
+        int broj1 = int.Parse(Console.ReadLine());
+        Console.Write("Unesite drugi broj: ");
+        int broj2 = int.Parse(Console.ReadLine());
+
+        // Provera koji broj je manji
+        if (broj1 > broj2)
+        {
+            int temp = broj1;
+            broj1 = broj2;
+            broj2 = temp;
+        }
+
+        for (int broj = broj1; broj <= broj2; broj++)
+        {
+            if (broj % 2 == 0)
+            {
+                Console.Write(broj + " ");
+            }
+        }
+    }
+}
